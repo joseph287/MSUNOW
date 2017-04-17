fc5ee278553193c734c5afae87b78533aa09db7f
Will Liu <Will Liu>
2017-04-17 10:26:42 -0400

    edit userinput activity

--- app/src/main/java/com/example/rasen/msunow/UserInputActivity.java
+++ app/src/main/java/com/example/rasen/msunow/UserInputActivity.java
@@ -1,5 +1,7 @@
 package com.example.rasen.msunow;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,19 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rasen.msunow.InputTopic.Topic;
+
+import java.util.Calendar;
+
+import static com.example.rasen.msunow.LoginActivity.user;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserInputActivity extends AppCompatActivity {
 
@@ -16,16 +31,28 @@ public class UserInputActivity extends AppCompatActivity {
     private EditText body;
     private String[] items;
     private Spinner spinner;
-    private String input_room;
+
+
     private String input_title;
     private String input_body;
+    private String input_room;
+    private String author;
+    private String time;
+    private String photoURL;
+    private int karma;
 
-
-
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private final String TOPIC="topic";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference(TOPIC);
+
+
 
         title = (EditText) findViewById(R.id.topic_title);
         body = (EditText) findViewById(R.id.topic_body);
@@ -41,7 +68,7 @@ public class UserInputActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-               input_room= items[position];
+                input_room = items[position];
             }
 
             @Override
@@ -52,12 +79,10 @@ public class UserInputActivity extends AppCompatActivity {
 
 
 
-
     }
 
     //intent to previous page
     public void BackToPre(View view) {
-
 
 
     }
@@ -69,7 +94,23 @@ public class UserInputActivity extends AppCompatActivity {
     //insert data to database
     public void post(View view) {
 
-        input_title= title.getText().toString();
-        input_body= body.getText().toString();
+        input_title = title.getText().toString();
+        input_body = body.getText().toString();
+        //this can be replaced by user in login
+        author = "user1@montclair.edu";
+        time = Calendar.getInstance().getTime().toString();
+        photoURL = null;
+        karma = 0;
+
+        Topic input = new Topic(input_title, input_body, input_room, author, time, photoURL, karma);
+        myRef.push().setValue(input);
+
+        startActivity(new Intent(this, topics_listview.class));
+
+
     }
+
 }
+
+
+
