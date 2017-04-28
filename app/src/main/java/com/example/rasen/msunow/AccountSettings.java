d8f53b6ac3176620f033db487986404afab064e4
Priyanka Phapale <pbphapale@gmail.com>
2017-04-28 01:25:39 -0400

    Add changes to update profile.

--- app/src/main/java/com/example/rasen/msunow/AccountSettings.java
+++ app/src/main/java/com/example/rasen/msunow/AccountSettings.java
@@ -1,26 +1,35 @@
 package com.example.rasen.msunow;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.rasen.msunow.Utils.Utils;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 
 /**
@@ -43,23 +52,23 @@ public class AccountSettings extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    private ArrayList<String> users;
     DatabaseReference myRef;
+    DatabaseReference userRef;
     FirebaseDatabase database;
     FirebaseAuth auth;
-    ChildEventListener mChildEventListener;
-    private SharedPreferences shprefs;
+    FirebaseUser user;
     private View root;
-    // TODO: Get current user in email
-    private String email = "user1@montclair.edu";
-    private AccountSettings accountSettingRequest;
-
+    private String currentUserEmail;
     private EditText acstFirstName;
     private EditText acstLastName;
     private EditText acstEmail;
     private EditText acstPassword;
     private EditText acstDoB;
-    private Button acstSubmit;
+    private Button acstUpdateChanges;
+
+    Map<String, String> activeUser;
+
+    Calendar myCalendar = Calendar.getInstance();
 
 
     public AccountSettings() {
@@ -105,54 +114,61 @@ public class AccountSettings extends Fragment {
 
     private void loadUI() {
 
-        users = new ArrayList<>();
         database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
         myRef = database.getReference("users");
+        user = auth.getCurrentUser();
+        currentUserEmail = user.getEmail();
+
 
         acstFirstName = (EditText) root.findViewById(R.id.acst_firstName);
         acstLastName = (EditText) root.findViewById(R.id.acst_lastName);
         acstEmail = (EditText) root.findViewById(R.id.acst_Email);
         acstPassword = (EditText) root.findViewById(R.id.acst_Password);
+
         acstDoB = (EditText) root.findViewById(R.id.acst_DOB);
-        acstSubmit = (Button) root.findViewById(R.id.acst_btn_save_settings);
-
-        //useremail.setText(getSharedPreferences(Utils.SHPRFN, MODE_APPEND).getString(Utils.CURRUSER, ""));
-
-        mChildEventListener = new ChildEventListener() {
+        acstDoB.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                RegisterRequest users = dataSnapshot.getValue(RegisterRequest.class);
-                if(users.getEmail().equals(email)) {
-                    acstFirstName.setText(users.getFirstName());
-                    acstLastName.setText(users.getLastName());
-                    acstEmail.setText(users.getEmail());
-                    acstPassword.setText(users.getPassword());
-                    acstDoB.setText(users.getDob());
+            public void onClick(View v) {
+                new DatePickerDialog(getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        acstUpdateChanges = (Button) root.findViewById(R.id.acst_btn_update_changes);
+        acstUpdateChanges.setOnClickListener(new UpdateChangesLsnr());
+
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                activeUser = getUsers((Map<String, Object>) dataSnapshot.getValue());
+                if (activeUser != null) {
+                    acstFirstName.setText(activeUser.get("firstName"));
+                    acstLastName.setText(activeUser.get("lastName"));
+                    acstEmail.setText(activeUser.get("email"));
+                    acstPassword.setText(activeUser.get("password"));
+                    acstDoB.setText(activeUser.get("dob"));
                 }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        };
-        myRef.addChildEventListener(mChildEventListener);
+        });
+    }
+
+    private Map<String, String> getUsers(Map<String, Object> user) {
+        ArrayList<String> users = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : user.entrySet()) {
+            Map<String, String> singleUser = (Map<String, String>) entry.getValue();
+            if (singleUser.get("email").equals(currentUserEmail)) {
+                return singleUser;
+
+            }
+        }
+        return null;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -192,5 +208,92 @@ public class AccountSettings extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private class UpdateChangesLsnr implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            myRef = database.getReference("users");
+            String updateUserId = activeUser.get("userId");
+
+            // get the user entered data in variable
+            final String firstName = acstFirstName.getText().toString().trim();
+            final String lastName = acstLastName.getText().toString().trim();
+            final String password = acstPassword.getText().toString().trim();
+            final String dob = acstDoB.getText().toString().trim();
+
+            clearErrors();
+            //validating the entered data
+            if (verifyData(firstName, lastName, password, dob)) {
+                userRef = myRef.child(updateUserId);
+                Map<String, Object> userMap = new HashMap<String, Object>();
+                userMap.put("firstName", firstName);
+                userMap.put("lastName", lastName);
+                userMap.put("password", password);
+                userMap.put("dob", dob);
+                userRef.updateChildren(userMap);
+            }
+            Toast.makeText(getContext(),"Values Updated",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDoB();
+        }
+    };
+
+    private void updateDoB() {
+
+        String myFormat = "MM/dd/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        acstDoB.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void clearErrors() {
+        acstFirstName.setError(null);
+        acstLastName.setError(null);
+        acstPassword.setError(null);
+        acstDoB.setError(null);
+    }
+
+    private boolean verifyData(String firstName, String lastName, String password, String dob) {
+        Boolean isValid = true;
+
+        // FirstName validation
+        if (TextUtils.isEmpty(firstName)) {
+            acstFirstName.setError("Please enter a first name");
+            isValid = false;
+        }
+
+        // LastName validation
+        if (TextUtils.isEmpty(lastName)) {
+            acstLastName.setError("Please enter your last name");
+            isValid = false;
+        }
+
+        // DoB validation
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        if (TextUtils.isEmpty(dob)) {
+            acstDoB.setError("Please select valid date of birth");
+            isValid = false;
+        }
+
+        // Password validation, minimum 6 characters required
+        String passwordPattern = "(?=.*[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$";
+        if (TextUtils.isEmpty(password) || !(password.matches(passwordPattern))) {
+            acstPassword.setError("Password should include alpha,no. & special characters with min length 6");
+            isValid = false;
+
+        }
+
+        return isValid;
     }
 }
